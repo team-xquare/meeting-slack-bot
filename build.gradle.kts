4bf4e7d8daa560a3b8728101f68fa8b9ca34e181
smoothbear <kjbin0420@naver.com>
2021-07-09 11:39:06 +0900

    :bug: :: (#3) Fix clean task from running script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("application")
 }
 
 group = "io.github.team-xquare"
@@ -10,8 +11,14 @@ repositories {
 }
 
 dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.slack.api:bolt-socket-mode:1.8.1")
     implementation("javax.websocket:javax.websocket-api:1.1")
     implementation("org.glassfish.tyrus.bundles:tyrus-standalone-client:1.17")
-    implementation(kotlin("stdlib"))
+    implementation("org.slf4j:slf4j-simple:1.7.31")
+}
+
+application {
+    mainClass.set("AppKt")
 }
