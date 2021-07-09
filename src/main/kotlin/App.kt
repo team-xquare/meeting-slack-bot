52b0d215fbafdf163a102f098415dd0c8c42ed8c
smoothbear <kjbin0420@naver.com>
2021-07-09 21:27:32 +0900

    ⚡️ :: (#2) Add schedule modal

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,15 @@
 import com.slack.api.bolt.App
 import com.slack.api.bolt.socket_mode.SocketModeApp
+import handler.AmsHandler
 
 fun main() {
     val app = App()
+    val amsHandler = AmsHandler()
+
+    app.command("/ams", amsHandler::addSchedule)
+    app.viewClosed("add-schedule") { _, ctx ->
+        ctx.ack()
+    }
 
     SocketModeApp(app).start()
 }
---
+++ src/main/kotlin/view/modal/ScheduleModal.kt
@@ -0,0 +1,78 @@
+package view.modal
+
+import com.slack.api.model.block.Blocks.*
+import com.slack.api.model.block.composition.BlockCompositions.option
+import com.slack.api.model.block.composition.BlockCompositions.plainText
+import com.slack.api.model.block.element.BlockElements.*
+import com.slack.api.model.view.View
+import com.slack.api.model.view.Views.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun buildScheduleModal(): View = view { view ->
+    view
+        .callbackId("add-schedule")
+        .type("modal")
+        .notifyOnClose(true)
+        .title(viewTitle { it.type("plain_text").text("회의 일정 추가").emoji(true) })
+        .submit(viewSubmit { it.type("plain_text").text("추가") })
+        .close(viewClose { it.type("plain_text").text("취소") })
+        .blocks(
+            asBlocks(
+                input { input ->
+                    input
+                        .label(
+                            plainText("회의 안건")
+                        )
+                        .element(
+                            plainTextInput {
+                                it
+                                    .actionId("meeting-agenda")
+                                    .placeholder(plainText("회의 안건(주제)를 입력해주세요"))
+                            }
+                        )
+                },
+                input { input ->
+                    input
+                        .label(
+                            plainText("참석자")
+                        )
+                        .element(
+                            multiUsersSelect {
+                                it
+                                    .actionId("attender-picker")
+                                    .placeholder(plainText("회의 참석자를 선택해주세요"))
+                            }
+                        )
+                },
+                input { input ->
+                    input
+                        .label(
+                            plainText("회의 날짜")
+                        )
+                        .element(
+                            datePicker {
+                                it
+                                    .actionId("date-picker")
+                                    .initialDate(SimpleDateFormat("yyyy-MM-dd").format(Date()))
+                            }
+                        )
+                },
+                input { input ->
+                    input
+                        .label(
+                            plainText("회의 시간")
+                        )
+                        .element(
+                            staticSelect { select ->
+                                select
+                                    .actionId("hour-picker")
+                                    .options(
+                                        listOf(0..24).map { i -> option { it.description(i).value(i) } }
+                                    )
+                            }
+                        )
+                }
+            )
+        )
+}
