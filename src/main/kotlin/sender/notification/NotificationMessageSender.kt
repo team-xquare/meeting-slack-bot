3f3194945bcc40da236588ede7f20a0ad96cd6e5
smoothbear <kjbin0420@naver.com>
2021-07-12 19:26:28 +0900

    :bug: :: Fix channel name to environment variable

--- src/main/kotlin/sender/notification/NotificationMessageSender.kt
+++ src/main/kotlin/sender/notification/NotificationMessageSender.kt
@@ -9,7 +9,7 @@ class NotificationMessageSender : MessageSender(), NotificationSender {
     override fun sendNotification(senderMeeting: SenderMeeting) {
 
         val response = methods.chatPostMessage { message -> message
-            .channel("#bot-testing")
+            .channel(System.getenv("SLACK_NOTIFICATION_CHANNEL"))
             .attachments(buildNotifyScheduleBlock(senderMeeting))
         }
 
